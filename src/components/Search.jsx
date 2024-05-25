14a8e0538fb5b23759c6775006675b850af9aa3e
Pradeep Kumar <pradeepwork2023@gmail.com>
2024-05-25 08:41:31 +0530

    Input Box Animation

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
 import Suggestions from "./Suggestions";
 import { MdOutlineSearch } from "react-icons/md";
-
-// Adding Redux
-import { useDispatch } from "react-redux";
-import { setLoader } from "../features/loader/loaderData";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Search = () => {
   // store input value state ❗❗❗❗❗
   const [inputValue, setInputValue] = useState("");
 
-  // redux dispatch
-  const dispatch = useDispatch();
+  // faking a loading animation whenever any data is fetching ❗❗❗❗❗
+  const [load, setload] = useState(false);
 
   // stores 5 suggestions for user ❗❗❗❗❗
   const [suggestions, setSuggestions] = useState([]);
 
   // fetching data using API by passing name ❗❗❗❗❗
   const fetchData = async (name) => {
-    dispatch(setLoader(true));
+    // This will change state of loader animation in input box
+    setload(true);
     let req = await fetch(`https://search.imdbot.workers.dev/?q=${name}`);
     let data = await req.json();
     let suggested = Array.from(data.description);
     suggested = suggested.slice(0, 5);
     setSuggestions([...suggested]);
-    dispatch(setLoader(false));
+    setload(false);
   };
 
   // handling input element values and fetching data if there are more than 3 letters in input box ❗❗❗❗❗
@@ -44,13 +42,18 @@ const Search = () => {
         {/* faking a loading animation whenever any data is fetching ❗❗❗❗❗ */}
         <div className="w-full flex justify-center">
           <div className="w-1/3 flex flex-col bg-white  rounded-l-md rounded-br-md relative">
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleChange}
-              className="h-14 w-full px-4 outline-none rounded-l-md"
-              placeholder="Search Movies..."
-            />
+            <div className="flex">
+              <input
+                type="text"
+                value={inputValue}
+                onChange={handleChange}
+                className="h-14 w-11/12 px-4 outline-none rounded-l-md"
+                placeholder="Search Movies..."
+              />
+              <div className="bg-white flex items-center px-2">
+                {load && <AiOutlineLoading3Quarters className="text-xl font-bold animate-spin" />}
+              </div>
+            </div>
 
             {/* if suggestions state has some value than this code will work and add component ❗❗❗❗❗*/}
             {suggestions.length > 0 && (
@@ -61,6 +64,9 @@ const Search = () => {
               />
             )}
           </div>
+          {/* <div className="bg-white flex items-center px-2">
+          <AiOutlineLoading3Quarters />
+          </div> */}
           <button className="h-14 bg-yellow-400 px-7 rounded-r-md">
             <MdOutlineSearch className=" text-4xl " />
           </button>
